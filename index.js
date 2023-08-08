228a462127c566eaf5af3c10b9b5f08e772ebf10
Sunil K Joseph <sunilkjoseph@mac.edu.in>
2023-08-08 22:01:40 +0530

    Empty message styling update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ onValue(shoppingListInDB, function(snapshot) {
             appendItemToShoppingListEl(currentItem)
         }    
     } else {
-        shoppingListEl.innerHTML = "No items here... yet"
+        shoppingListEl.innerHTML = `<li id="uniqueItem">Cart is Empty!</li>`
     }
 })
 
